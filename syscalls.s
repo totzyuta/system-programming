50f5c6d077c237efe9456c8e986c46941a5f1099
totzYuta <zfbfmtmxaz@gmail.com>
2014-12-04 14:18:28 +0900

    Declaration of syscalls functions

--- syscalls.s
+++ syscalls.s
@@ -12,10 +12,38 @@ _print_int:
   addu  $sp, $sp, 24
   j     $ra
 
+
 _print_string:
+  subu  $sp, $sp, 24
+  sw    $ra, 20($sp)
+
+  li    $v0, 4  # 4: print_string
+  syscall
+
+  lw    $ra, 20($sp)
+  addu  $sp, $sp, 24
+  j     $ra
+
 
 _read_int:
+  subu  $sp, $sp, 24
+  sw    $ra, 20($sp)
+
+  li    $v0, 5  # 5: read_int
+  syscall
+
+  lw    $ra, 20($sp)
+  addu  $sp, $sp, 24
+  j     $ra
+
 
 _read_string:
+  subu  $sp, $sp, 24
+  sw    $ra, 20($sp)
 
+  li    $v0, 8  # 8: read_string
+  syscall
 
+  lw    $ra, 20($sp)
+  addu  $sp, $sp, 24
+  j     $ra
