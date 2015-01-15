97fcf3cf571712ca3c0f963cc5dcd5243805409e
totz <yuta.totz@gmail.com>
2015-01-15 12:28:42 +0900

    [Modify] Modified myprintf() function by the example

--- myprintf/myprintf.c
+++ myprintf/myprintf.c
@@ -1,26 +1,39 @@
 void myprintf(char *fmt, ...) {
-  printf("Format =>  %s\n", fmt);
-  char *p = ((char*)&fmt) + 8;
-  printf("Second Argument => %s\n", p);
+  // printf("Format =>  %s\n", fmt);
+  // char *p = ((char*)&fmt) + 8;
+  // printf("Second Argument => %s\n", p);
+  
+  int i, argc = 0;
+  char *s;
+
   while (*fmt) {
     if (*fmt == '%') {
       fmt++;
+      argc++;
       switch (*fmt) {
         case '%':
           // Process of %%
+          break;
         case 'c':
           // Process of %c
+          break;
         case 'd':
           // Process of %d
+          i = *((int*) ((char *)&fmt + argc * sizeof(void *)) );
+          print_int(i);
+          break;
         case 'u':
           // Process of %u
+          break;
         case 's':
           // Process of %s
+          s = *((int*) ((char *)&fmt + argc * sizeof(void *)) );
+          break;
         break;
       }
     }else {
       // print a character as it is 
-      // print_character(fmt);
+      print_char(*fmt);
     }
     fmt++;
   }
