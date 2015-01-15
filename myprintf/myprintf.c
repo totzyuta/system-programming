327ec03701cfe4e1772a2968649f2feadd8001bf
totz <yuta.totz@gmail.com>
2015-01-15 14:26:24 +0900

    [add] Add some new format for myprintf #4

--- myprintf/myprintf.c
+++ myprintf/myprintf.c
@@ -18,21 +18,53 @@ void myprintf(char *fmt, ...) {
       argc++;
       switch (*fmt) {
         case '%':
-          // Process of %%
+          // Process of %%: print '%'
+          print_char(*fmt);
           break;
         case 'c':
-          // Process of %c
+          // Process of %c: print character
+          c = *((int*) ((char *)&fmt + argc * sizeof(void *)) );
+          print_char(c);
           break;
         case 'd':
-          // Process of %d
+          // Process of %d: print integer 
           i = *((int*) ((char *)&fmt + argc * sizeof(void *)) );
           print_int(i);
           break;
-        case 'u':
-          // Process of %u
+        case 'x':
+          // TODO: Process of %x: print as hexadecimal number
+          i = *((int*) ((char *)&fmt + argc * sizeof(void *)) );
+          str = (char *) malloc(15);
+          while(i) {
+            str = i % 16;
+            if(10>*str){
+              *str+=0x30;     //0x30は0の文字コード
+            }else{
+              *str+=(0x41-0x0A);  //0x41はAの文字コード
+            }
+            i/=16;
+          }
+          print_string(str);
+          // printf("0x%s\n",str_ptr);
+          break;
+        case 'o':
+          // TODO: Process of %o: print as octal number
+          i = *((int*) ((char *)&fmt + argc * sizeof(void *)) );
+          str = (char *) malloc(15);
+          while(i) {
+            str = i % 8;
+            if(10>*str){
+              *str+=0x30;     //0x30は0の文字コード
+            }else{
+              *str+=(0x41-0x0A);  //0x41はAの文字コード
+            }
+            i/=8;
+          }
+          print_string(str);
+          // printf("0x%s\n",str_ptr);
           break;
         case 's':
-          // Process of %s
+          // Process of %s: print string
           s = *((int*) ((char *)&fmt + argc * sizeof(void *)) );
           print_string(s);
           break;
